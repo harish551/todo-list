2c3a84a00a98b969690a13e8bb383f7f3a10fe22
Marri Harish <marri.harish@cosmiclabs.co>
2019-10-16 16:59:10 +0530

    Added localStorage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
+import logger from 'redux-logger'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
+import { loadState, saveState } from './localStorage'
 import App from './App'
 
-const store = createStore(rootReducer)
+
+const persistedState = loadState();
+const store = createStore(
+  rootReducer,
+  persistedState,
+  applyMiddleware(logger)
+);
+
+store.subscribe(() => {
+  saveState({
+    todos: store.getState().todos
+  });
+});
 
 render(
   <Provider store={store}>
